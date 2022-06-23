a6602383030a77f45b51c176eb6c7cd283b9e764
Quang Khanh Hua <khanhhua@comscitech.onmicrosoft.com>
2022-06-23 22:49:57 +0800

    add go to top button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 alert("This website is still in the development period, proceed with caution!");
+button = document.getElementById("top-btn");
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
