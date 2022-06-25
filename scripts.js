b3e01f2001a19c0bfe8af616ca841d60f76fbb3c
Quang Khanh Hua <khanhhua@comscitech.onmicrosoft.com>
2022-06-25 21:46:44 +0800

    change font family and font size

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-alert("This website is still in the development period, proceed with caution!");
+//alert("This website is still in the development period, proceed with caution!");
 button = document.getElementById("top-btn");
 window.onscroll = function () {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        button.style.display = "block";
+        button.style.display = "box";
     }
     else {
         button.style.display = "none";
